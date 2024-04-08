6d0fd7d17bb6fa947633a4051f6fd04b639afccd
haozhe haozhe <haozhe@g-12-l0002.cm.cluster>
2024-04-09 00:55:40 +0200

    update the script to get abundance

--- 209_abundance_per_sample.py
+++ 209_abundance_per_sample.py
@@ -1,46 +1,40 @@
+import os
 import gzip
 import argparse
-from pathlib import Path
 
-def process_files(input_file, input_file2, output_file, threshold):
-    dictlt = {}
-    with gzip.open(input_file, 'rt') as infile:
-        for line in infile:
-            lst = line.strip().split()
-            if float(lst[1]) > threshold:
-                dictlt[lst[0]] = lst[0]
+def process_files(input_folder, output_folder, threshold):
+    for file_name in os.listdir(input_folder):
+        if file_name.endswith("_coverage_info.txt.gz"):
+            sample_name = file_name[:-21]  # 去掉后缀 "_coverage_info.txt.gz"
+            coverage_file = os.path.join(input_folder, file_name)
+            profile_file = os.path.join(input_folder, sample_name + "_profile_rb.txt")
+            output_file = os.path.join(output_folder, sample_name + "_existing_contigs_ab_per_sample.txt")
 
-    with open(input_file2, 'rt') as infile, open(output_file, 'w') as outfile:
-        for i in range(9):
-            infile.readline()
-        for line in infile:
-            lst = line.strip().split()
-            try:
-                st = "{}\t{}\n".format(dictlt[lst[0]], lst[1])
-                outfile.write(st)
-            except KeyError:
-                st = "{}\t{}\n".format(lst[0], 0)
-                outfile.write(st)
+            dictlt = {}
+            with gzip.open(coverage_file, 'rt') as infile:
+                for line in infile:
+                    lst = line.strip().split()
+                    if float(lst[1]) > float(threshold):
+                        dictlt[lst[0]] = lst[0]
 
-def find_pairs(input_dir, threshold):
-    input_dir = Path(input_dir)
-    coverage_files = list(input_dir.glob('*_coverage_info.txt.gz'))
-    profile_files = {f.stem.rstrip('_coverage_info'): f for f in coverage_files}
-    
-    for profile_base, coverage_file in profile_files.items():
-        profile_file = input_dir / (profile_base + '_profile_rb.txt')
-        if profile_file.exists():
-            output_file = input_dir / (profile_base + '_existing_contigs_ab_per_sample.txt')
-            process_files(str(coverage_file), str(profile_file), str(output_file), threshold)
-
-def main():
-    parser = argparse.ArgumentParser(description="Process files in pairs with a given threshold.")
-    parser.add_argument("-d", "--directory", required=True, help="Directory containing the input files.")
-    parser.add_argument("-t", "--threshold", type=float, required=True, help="Threshold for contig detection.")
-    args = parser.parse_args()
-
-    find_pairs(args.directory, args.threshold)
+            with gzip.open(profile_file, 'rt') as infile:
+                with open(output_file, 'w') as outfile:
+                    for i in range(9):
+                        infile.readline()
+                    for line in infile:
+                        lst = line.strip().split()
+                        try:
+                            st = "{}\t{}\n".format(dictlt[lst[0]], lst[1])
+                            outfile.write(st)
+                        except:
+                            st = "{}\t{}\n".format(lst[0], 0)
+                            outfile.write(st)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process coverage and profile files.")
+    parser.add_argument("-i", "--input", required=True, help="Input folder containing coverage and profile files.")
+    parser.add_argument("-o", "--output", required=True, help="Output folder for processed files.")
+    parser.add_argument("-t", "--threshold", type=float, default=0.0, help="Threshold value for coverage (default: 0.0).")
+    args = parser.parse_args()
 
+    process_files(args.input, args.output, args.threshold)
